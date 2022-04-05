f6bf5e97bb5b2c98ae91a495c382ba9253996e43
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-05 12:21:39 +0100

    component hierarchy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
+
+const Square = () => {
+  return (
+    <div style={{
+      background: 'gold',
+      margin: 10,
+      padding: 5,
+    }}>
+      Square
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div style={{
+      background: 'skyblue',
+      margin: 10,
+      padding: 5,
+    }}>
+      Board
+      <Square/>
+    </div>
+  );
+};
 
 const Game = () => {
   return (
-    <div>
+    <div style={{
+      background: 'salmon',
+      margin: 10,
+      padding: 20,
+    }}>
       Game
+      <Board/>
     </div>
   );
 };
